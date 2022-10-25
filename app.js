d52ccf7113aff16539fc77746c1d806797f5708c
Kimseonghee <kimsunghee625@gmail.com>
2022-10-26 00:27:49 +0900

    justgram node https server test finished

--- app.js
+++ app.js
@@ -6,14 +6,6 @@ dotenv.config();
 const { validateToken } = require("./middlewares/validateToken");
 const router = require("./routes");
 
-// const createUserController = require("./controllers/createUserController");
-// const loginUserController = require("./controllers/loginUserCotroller");
-// const addPostController = require("./controllers/addPostController");
-// const postListController = require("./controllers/postListController");
-// const postChangeController = require("./controllers/postChangeController");
-// const removePostController = require("./controllers/removePostController");
-// const userPostController = require("./controllers/userPostController");
-
 const app = express();
 app.use(express.json()); //req.body undefined 에러 해결(아마 express사용시 발생하는 에러인듯? 전에는 body-parser Install해서 해결한 기억이 있는데 그게 express 업데이트 되면서 express내장 기능으로 추가 된듯)
 app.use(
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const http = require("http");
+const https = require("https");
+const fs = require("fs");
 const app = require("./app");
 
+const PORT = 3000;
+
+const options = {
+  key: fs.readFileSync("./key.pem", "utf-8"),
+  cert: fs.readFileSync("./cert.pem", "utf-8"),
+};
+
 const server = http.createServer(app);
+const httpsServer = https.createServer(options, app);
 
 const start = () => {
   try {
-    server.listen(3000, () => {
-      console.log("start server http://localhost:3000/");
+    httpsServer.listen(PORT, () => {
+      console.log(
+        "start server http://localhost:3000/ https://localhost:3000/"
+      );
     });
   } catch (err) {
     console.log(err);
